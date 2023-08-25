fa7dbbbe51761f14597b058fb26441436e00526b
Gyan Prakash <gyanprakash@microsoft.com>
2023-08-25 10:04:41 +0530

    some change

--- ParkingLot/src/Services/ExitGateService.java
+++ ParkingLot/src/Services/ExitGateService.java
@@ -9,6 +9,7 @@ import java.util.Objects;
 public class ExitGateService {
 
     ParkingFeeCalculationStrategy parkingFeeCalculationStrategy;
+    PaymentService paymentService;
 
     public ExitGateService(ParkingFeeCalculationStrategy parkingFeeCalculationStrategy){
         this.parkingFeeCalculationStrategy = parkingFeeCalculationStrategy;
@@ -20,7 +21,7 @@ public class ExitGateService {
     public void unParkVehicle(Vehicle vehicle, Ticket ticket) {
         if(Objects.equals(vehicle.getVehicleNumber(), ticket.getVehicleNumber())){
             double totalAmount = parkingFeeCalculationStrategy.calculateFee(ticket);
-
+            paymentService.makePayment(totalAmount);
         }
     }
 }
